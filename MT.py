135bb104071460b4d36a10c0b65791d7583abff7
Sébastien F <sebastien.fernandez@ens.uvsq.fr>
2022-12-21 14:29:29 +0100

    avancement fonction ouvrir fichier

--- MT.py
+++ MT.py
@@ -5,23 +5,33 @@ class MT:
         self.fichier = fichier
         self.etat_accept = ""
         self.etat_courant = ""
-        self.etat_bandes = []
+        self.etat_bandes = list(mot)
         self.position_tetes = []
+        self.transition = {}
         self.ouvrir_fichier()
 
     def ouvrir_fichier(self):
+        cpt = 0
         with open(self.fichier, "r") as f:
+            temp = ""
             for line in f:
-                
-                
-                
+                line = line.strip('\n')
+                if cpt == 0:
+                    self.etat_courant = line
+                elif cpt == 1:
+                    self.etat_accept = line
+                elif temp == "" and line != "":
+                    self.transition[line] = ""
+                    temp = line
+                elif line != "":
+                    self.transition[temp] = line
+                    temp = ""
+                cpt += 1
 
 
 
 
 
 
-
-
-
-mt1 = MT("1110","fichier1.txt")
+mt1 = MT("1110","mt1.txt")
+print(mt1.transition)
